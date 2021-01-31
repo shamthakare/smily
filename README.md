ae5762e085eb5cdb5c6984b79ce6ef94319d2539
shamthakare <shamthakare1992@gmail.com>
2021-01-31 16:57:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-agbmb4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-agbmb4)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+// appDiv;
+// const appDiv =
+document.getElementById("app");
+// .innerHTML = `<h1></h1>`;
+// const le =
+document.getElementById("le");
